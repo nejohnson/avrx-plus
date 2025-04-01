9a7fb522156e67279d375570bc0cbb37ed0541e9
Neil Johnson <neil.johnson71@gmail.com>
2025-04-01 21:31:24 +0100

    Update internal funcs
    Source tidy up.

--- src/axp_removeobj.c
+++ src/axp_removeobj.c
@@ -1,49 +1,6 @@
 /* Copyright (C) 2025 Neil Johnson */
 #include "axpluskernel.h"
 
-/*****************************************************************************
- *
- *  FUNCTION
- *      _avrxRemoveObject
- *
- *  SYNOPSIS
- *      pProcessID _avrxRemoveObject(pProcessID pQueue, pProcessID pObject)
- *
- *  DESCRIPTION
- *      Walks the Queue looking for the Object, then removes it.
- *
- *      Before:
- *
- *          pQueue              pObject          pNext
- *       +---------+          +---------+     +---------+
- *       |   next  +--/.../-->|   next  +---->|   next  +----->NOPID
- *       +---------+          +---------+     +---------+
- *       |   ...   |          |   ...   |     |   ...   |
- *       +---------+          +---------+     +---------+
- *
- *      After:
- *
- *          pQueue               pNext (returned)
- *       +---------+          +---------+
- *       |   next  +--/.../-->|   next  +----->NOPID
- *       +---------+          +---------+
- *       |   ...   |          |   ...   |
- *       +---------+          +---------+
- 
- *         pObject
- *       +---------+
- *       |   next  +----->NOPID
- *       +---------+
- *       |   ...   |
- *       +---------+
- *
- *
- *  RETURNS
- *      Pointer to next object in the Queue, else NOPID if not found or there
- *      is no next object.
- *
- *****************************************************************************/
-
 axpPID _axpRemoveObject(axpPID pQueue, axpPID pObject)
 {
    while (pQueue->next)
